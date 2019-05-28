d1d7c4d709db516f536a69d6feb074c83475c992
sameerpande12 <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-05-28 14:39:16 +0800

    added runner script

--- RScripts/runner.sh
+++ RScripts/runner.sh
@@ -4,7 +4,7 @@ gcc -Wall -o prober ./probe.c -lnfnetlink -lnetfilter_queue -lpthread -lm
 ip="$(ifconfig | grep -A 1 'ingress' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 echo $ip
 sudo iptables -I INPUT -p tcp -d $ip -m state --state ESTABLISHED -j NFQUEUE --queue-num 0
-sudo ./probe "$1" 2000 3000 1500 0 143 48 19 -1
+sudo ./prober "$1" 2000 3000 1500 0 143 48 19 -1
 sudo killall wget
 rm -f index*
 sudo iptables --flush
