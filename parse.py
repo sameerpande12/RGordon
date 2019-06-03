3bf4738f08ed46063bd3b1a6c6a2cedfc23d21fd
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-06-03 18:57:13 +0800

    some input data

---
+++ parse.py
@@ -0,0 +1,27 @@
+file =  open('sanitized-links.csv','r')
+count = 0
+mlist = []
+while True:
+    line = file.readline()
+    if not line:
+        break
+    mlist.append((count,line.strip().split(',')))
+    count=count+1
+
+myval = True
+str1=""
+str2=""
+ofile=open("output.txt","a+")
+for tup in mlist:
+    str1 = str(tup[0])
+    str2=""
+    i=3
+    j=len(tup[1])-2
+    while(i <= j):
+        str2+=tup[1][i]
+        if(i<j):
+            str2+=','
+        i=i+1
+        ofile.write(str1+" "+str2+"\n")
+ofile.close()
+file.close()
