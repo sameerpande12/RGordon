c824402897fee558851884eecc73b725c27fad64
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-06-10 14:11:16 +0800

    updated dummyTest.js

--- RScripts/dummyTest.js
+++ RScripts/dummyTest.js
@@ -17,7 +17,7 @@ const evaluate = function(startRTT, endRTT, emuDrop, chances_left, trials, cwnd,
     console.log("Entering "+rnum);
     const pyProg= spawn('python3',["calculate.py",url,trials,sigma_cwnd,cwnd,rnum,emuDrop]);
     pyProg.stderr.on('data', (data) => {
-      //console.log(`stderr: ${data}`);
+      console.log(`stderr: ${data}`);
     });
     pyProg.on('close', (code) => {
       console.log("calculate.py done for "+rnum);
@@ -28,13 +28,31 @@ const evaluate = function(startRTT, endRTT, emuDrop, chances_left, trials, cwnd,
       var values = [];
       tmp.forEach( function(str){values.push(parseInt(str));});
       var postData ='';
-      if(values[0]==0 && values[1]==0){
-        postData = { json: {last_error:"error",last_rtt_done:rnum.toString(),url:url,chances_left:(chances_left-1).toString(),viewpoint:viewPoint } };
-        path = '/api/worker/updateError';
-      }
-      else if(values[1]==0){
-        postData = { json: {last_rtt_done:rnum.toString(),url:url,viewpoint:viewPoint} };
-        path = '/api/worker/complete';
+      if( values[1]==0){
+        var isError = true;
+        for( iter = 0;iter<trials;iter++){
+                  var content;
+                  try{
+                            var statusCode = ((fs.readFileSync("./stats/status"+iter,"utf-8")).split("\n"))[0];
+                            statusCode = parseInt(statusCode);
+                            if(statusCode == 0){
+                              isError = false;
+                              break;
+                            }
+                  }
+                  catch(err){
+                        console.log(err);
+                  }
+        }
+        //if(isError){
+            chances_left = chances_left -1;
+            postData = { json: {last_error:"error",last_rtt_done:rnum.toString(),url:url,chances_left:(chances_left).toString(),viewpoint:viewPoint } };
+            path = '/api/worker/updateError';
+        //}
+        /*else{
+          postData = { json: {last_rtt_done:rnum.toString(),url:url,viewpoint:viewPoint} };
+          path = '/api/worker/complete';
+        }*/
       }
       else{
         path = '/api/worker/update';
@@ -49,8 +67,6 @@ const evaluate = function(startRTT, endRTT, emuDrop, chances_left, trials, cwnd,
 
         postData = { json: { cwnd: values[1].toString(), sigma_cwnd: values[0].toString(),last_rtt_done:values[2].toString(),url:url,emudrop:emuDrop.toString(),viewpoint:viewPoint } };
       }
-      //fs.appendFile("time.txt","\n",(err)=>{});
-      //fs.appendFile("time.txt",Date.now() - start,(err)=>{});
 
       console.log(postData.json);
       request.post(
@@ -68,8 +84,8 @@ const evaluate = function(startRTT, endRTT, emuDrop, chances_left, trials, cwnd,
           }
           //useloop = true;
           rnum++;
-          if(rnum<=endRTT){
-            evaluate();
+          if(rnum<=endRTT && values[1]!=0){
+            evaluate(startRTT, endRTT, emuDrop, chances_left, trials, cwnd, sigma_cwnd, url, rnum, path, evaluate);
           }
         }
       );
@@ -83,41 +99,47 @@ const evaluate = function(startRTT, endRTT, emuDrop, chances_left, trials, cwnd,
 };
 
 var pingServer = function(){
-  console.log("Every 2 seconds");
-  if(isFree){
-    isFree = false;
-    var path = '';
-    console.log("Asking for a job");
-    request.post(domain+'/api/worker/job',{json:{viewpoint:viewPoint}} ,function(error,resp,body)  {
-         if(typeof resp != 'undefined'){
-                var body=resp.body;
-                console.log(body);
-                if(body.message == "JOB"){
-                  var startRTT = parseInt(body.data[0].startRTT);
-                  var endRTT = parseInt(body.data[0].endRTT);
-                  var emuDrop = parseInt( body.data[0].start_emudrop);
-                  var chances_left = parseInt(body.data[0].chances_left);
-                  var trials = parseInt(body.data[0].trials);
-                  var cwnd = parseInt(body.data[0].cwnd);
-                  var sigma_cwnd = parseInt(body.data[0].sigma_cwnd);
-                  var url = body.data[0].url;
-                  var rnum = startRTT;
-                  //var useloop = true;
-                  console.log("python3 calculate.py"+url + " "+ trials+ " "+sigma_cwnd + " "+cwnd + " "+rnum +" "+ emuDrop);
-                  evaluate(startRTT, endRTT, emuDrop, chances_left, trials, cwnd, sigma_cwnd, url, rnum, path, evaluate);
-                }else{
-                  console.log("no Job---no Job");
-                  isFree = true;
+  //console.log("Every 2 seconds");
+  try{
+          if(isFree){
+            isFree = false;
+            var path = '';
+            console.log("Asking for a job");
+            request.post(domain+'/api/worker/job',{json:{viewpoint:viewPoint}} ,function(error,resp,body)  {
+                 if(typeof resp != 'undefined'){
+                        var body=resp.body;
+                        console.log(body);
+                        if(body.message == "JOB"){
+                          var startRTT = parseInt(body.data[0].startRTT);
+                          var endRTT = parseInt(body.data[0].endRTT);
+                          var emuDrop = parseInt( body.data[0].start_emudrop);
+                          var chances_left = parseInt(body.data[0].chances_left);
+                          var trials = parseInt(body.data[0].trials);
+                          var cwnd = parseInt(body.data[0].cwnd);
+                          var sigma_cwnd = parseInt(body.data[0].sigma_cwnd);
+                          var url = body.data[0].url;
+                          var rnum = startRTT;
+                          //var useloop = true;
+                          console.log("python3 calculate.py"+url + " "+ trials+ " "+sigma_cwnd + " "+cwnd + " "+rnum +" "+ emuDrop);
+                          evaluate(startRTT, endRTT, emuDrop, chances_left, trials, cwnd, sigma_cwnd, url, rnum, path, evaluate);
+                        }else{
+                          console.log("no Job---no Job");
+                          isFree = true;
+                        }
                 }
-        }
-        else{
-          isFree = true;
-        //  console.log("No repsonse");
-        }
-    }).on("error", (err) => {
-     console.log("Error: " + err.message);
+                else{
+                  isFree = true;
+                //  console.log("No repsonse");
+                }
+            }).on("error", (err) => {
+             console.log("Error: " + err.message);
 
-   });
-  }
+           });
+          }
+    }
+    catch(err){
+      isFree = true;
+      console.log(err);
+    }
 }
 setInterval(pingServer,2000);
--- dummyServer/app.js
+++ dummyServer/app.js
@@ -5,12 +5,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-/*var sigma_cwnd = 1437;
-var cwnd = 103;
-var emuDrop = 96;
-var rtt = 27;
+
+/*
+var sigma_cwnd = 3040;
+var cwnd = 71;
+var emuDrop = 167;
+var rtt = 1;
 var assigned = true;
 */
+
 
 var sigma_cwnd = 0;
 var cwnd = 0;
@@ -25,12 +28,15 @@ app.post('/api/worker/job',function(req,res){
   res.json({
     message:'JOB',
     //url:"www.google.co.in/search?q=Valdemar+Poulsen&sa=X&hl=en&tbm=isch&source=iu&ictx=1&fir=AkQRON4e7zgjWM%253A%252Ch3kyesQBUnEicM%252C_&usg=AI4_-kSPFA-FLXL_4qaZP2B7aL3UDKH2Ew&ved=2ahUKEwjRgfT4tereAhXCb30KHQL9DEgQ_h0wEnoECAYQCA#imgrc=_",
-    data:[{url:"https://www.youtube.com",
+    data:[{
+         url:"https://www.youtube.com",
+      //url:" https://www.google.com.ph/search?q=Valdemar+Poulsen&sa=X&hl=en&tbm=isch&source=iu&ictx=1&fir=AkQRON4e7zgjWM%253A%252Ch3kyesQBUnEicM%252C_&usg=AI4_-kSPFA-FLXL_4qaZP2B7aL3UDKH2Ew&ved=2ahUKEwjRgfT4tereAhXCb30KHQL9DEgQ_h0wEnoECAYQCA&gws_rd=ssl",
+      //url:"https://sg.yahoo.com/?p=us",
     viewpoint:req.body.viewpoint,
     sigma_cwnd:sigma_cwnd.toString(),
     cwnd:cwnd.toString(),
     startRTT:rtt.toString(),
-    endRTT:(rtt).toString(),
+    endRTT:(rtt+3).toString(),
     trials:trials.toString(),
     start_emudrop:emuDrop.toString(),
     chances_left:chancesLeft.toString()
@@ -82,7 +88,7 @@ app.post('/api/worker/updateError',function(req,res){
 
 
 app.post('/api/worker/complete',function(req,res){
-  console.log("received");
+  console.log("received-> COMPLETE");
   console.log(req.body);
      sigma_cwnd = 0;
      cwnd = 0;
