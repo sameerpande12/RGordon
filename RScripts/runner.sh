7d01382abc067b463a1d1780529e1670b34c37aa
Sameer <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-06-14 15:58:55 +0800

    flusing iptables

--- RScripts/runner.sh
+++ RScripts/runner.sh
@@ -3,6 +3,7 @@ ip="$(ifconfig | grep -A 1 'ingress' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f
 echo $i
 sudo iptables -I INPUT -p tcp -d $MAHIMAHI_BASE -m state --state ESTABLISHED -j NFQUEUE --queue-num $7
 sudo ./prober "$1" 2000 3000 1500 $2 $3 $4 $5 $6 $7
+sudo iptables --flush
 #$7 is the job id
 #$2 is the trial number
 #since now
