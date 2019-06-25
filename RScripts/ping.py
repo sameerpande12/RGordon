edd91f26df283fe40ceaa3d21d8c9e3a80eeff1e
Sameer Pande <44154408+sameerpande12@users.noreply.github.com>
2019-06-25 16:52:30 +0800

    Update ping.py

--- RScripts/ping.py
+++ RScripts/ping.py
@@ -15,7 +15,7 @@ domain = 'http://10.255.255.1:4000'
 # domain = 'http://137.132.83.199:4000'
 # domain = 'http://localhost:3000'
 # domain='http://172.26.191.175:4000'
-numParallelJobs=5
+numParallelJobs=12
 
 path="/api/worker/job"
 #isFree=True;
