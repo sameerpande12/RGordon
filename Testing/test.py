dd182c58be833b4247c98640df70cbfc15412e26
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-07-02 12:16:52 +0700

    updating test

--- Testing/test.py
+++ Testing/test.py
@@ -75,10 +75,7 @@ def runJob(url_id,profile_id):
     subprocess.call([command],shell=True,executable='/bin/bash')
     os.chdir("..")
 
-for i in range(2):
-    if( i is not 0):
-        runJob(i+2,3)
-        runJob(i+2,4)
-        runJob(i+2,5)
-        runJob(i+2,6)
-    runJob(i+2,7)
+for i in range(11):
+    if(i>3):
+        for j in range(5):
+            runJob(i,j+3)
