59cbdd958895d04774bd8851d9c575d8dc6eb527
Sameer <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-07-07 11:53:18 +0800

    added nfqueue for simple probing as well

--- RScripts/mtuHelper.sh
+++ RScripts/mtuHelper.sh
@@ -1,2 +1,5 @@
 sudo ifconfig ingress mtu $1
+ip="$(ifconfig | grep -A 1 'ingress' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+sudo iptables -I INPUT -p tcp -d $ip -m state --state ESTABLISHED -j NFQUEUE --queue-num $4
 wget -q --no-check-certificate -t $3 -U 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:62.0) Gecko/20100101 Firefox/62.0' -O index$4.html -T 10 "$2"
+sudo iptables --flush
