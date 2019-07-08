1e3bf1d317d7ce924902fca84ab5adfb10dd81ed
Sameer <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-07-08 12:22:10 +0800

    temporary code

--- RScripts/runner.sh
+++ RScripts/runner.sh
@@ -7,6 +7,7 @@ ip="$(ifconfig | grep -A 1 'ingress' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f
 sudo iptables -I INPUT -p tcp -d $ip -m state --state ESTABLISHED -j NFQUEUE --queue-num $7
 sudo ./prober "$1" 2000 3000 1500 $2 $3 $4 $5 $6 $7 & PID=`echo $!`
 echo $PID >> indexPages$7/pids.txt
+wait $PID
 sudo iptables --flush
 #$7 is the job id
 #$2 is the trial number
