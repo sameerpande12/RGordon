1d9708dd399a242bc44558efe7e19e51b46bb089
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-07-15 17:47:24 +0700

    updating graphs

--- Unknown_Website_Prober/Observations/plot.gnuplot
+++ Unknown_Website_Prober/Observations/plot.gnuplot
@@ -3,4 +3,5 @@ set output "Plots/".index."/".domain."/".filename.".png"
 set title filename."    BDP_1-".bdp1." packets     BDP_2-".bdp2." packets     Drop-".drop."     mm-delay-".mmd." ms    delay_1-".delay1." us    delay_2-".delay2." us"
 set ylabel "cwnd"
 set xlabel "RTT"
+if(x!=-1)set label 1 "" at x,y point pointtype 2
 plot "Data/".filename.".csv" u 3:2 w l notitle
--- Unknown_Website_Prober/Observations/plot.sh
+++ Unknown_Website_Prober/Observations/plot.sh
@@ -8,4 +8,18 @@ delay2=$7
 index=$8
 domain=$9
 mkdir -p Plots/$index/$domain
-gnuplot -e "data_path='Data/';filename='$filename'; bdp1='$bdp1';bdp2='$bdp2' ; drop='$drop' ; mmd='$mmd' ; delay1='$delay1' ; delay2='$delay2'; index='$index';domain='$domain'" plot.gnuplot
+input=Data/$filename.csv
+y='-1'
+x='-1'
+while IFS= read -r line
+do
+  z=`echo $line|cut -d ' ' -f 1`
+  if [ $z -gt 1500 ]
+  then
+    y=`echo $line|cut -d ' ' -f 2`
+    x=`echo $line|cut -d ' ' -f 3`
+    break
+  fi
+done < "$input"
+
+gnuplot -e "data_path='Data/';filename='$filename'; bdp1='$bdp1';bdp2='$bdp2' ; drop='$drop' ; mmd='$mmd' ; delay1='$delay1' ; delay2='$delay2'; index='$index';domain='$domain';x='$x';y='$y'" plot.gnuplot
