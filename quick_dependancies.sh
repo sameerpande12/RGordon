e1574371fd1fc446476b22a3e0a72830184d03a1
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-07-15 18:21:00 +0700

    temporary update quick_dependancies.sh

--- quick_dependancies.sh
+++ quick_dependancies.sh
@@ -13,7 +13,7 @@ sudo apt-get install -y mahimahi
 sudo dpkg-reconfigure -p critical dash
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo apt-get install -y libnetfilter-queue-dev
-sudo apt-get install tmux
+sudo apt-get install screen
 
 # apt-get install sudo -y   # for Docker
 sudo apt-get install -y iputils-ping
@@ -21,8 +21,8 @@ sudo apt-get install -y wget
 sudo apt-get install -y psmisc
 sudo apt install -y net-tools
 
-sudo apt-get install -y curl
-sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo bash
-sudo apt-get install -y nodejs
-
-npm install
+# sudo apt-get install -y curl
+# sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo bash
+# sudo apt-get install -y nodejs
+#
+# npm install
