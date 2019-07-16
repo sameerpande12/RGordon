9fbf25413156c95453eb82637afeddc8baf292c9
Atishya-Jain <atishya.jain@rocketmail.com>
2019-07-16 17:42:40 +0800

    Docker Files

--- quick_dependancies.sh
+++ quick_dependancies.sh
@@ -4,13 +4,12 @@ sudo apt-get install -y python3-pip
 sudo apt install -y python3-pip
 pip3 install pandas
 pip3 install numpy
-pip3 install matplotlib
-# python3 -m pip install numpy
-# python3 -m pip install pandas
-# python3 -m pip install matplotlib
+python3 -m pip install numpy
+python3 -m pip install pandas
+python3 -m pip install matplotlib
 pip3 install --upgrade pip
-pip install --user matplotlib
-#sudo python3-pip install numpy
+sudo -H pip3 install pandas
+sudo apt-get install python3-pandas
 sudo add-apt-repository ppa:keithw/mahimahi -y
 sudo apt-get update
 sudo apt-get install -y mahimahi
@@ -19,14 +18,15 @@ sudo sysctl -w net.ipv4.ip_forward=1
 sudo apt-get install -y libnetfilter-queue-dev
 sudo apt-get install screen
 
-# apt-get install sudo -y   # for Docker
+apt-get install sudo -y   # for Docker
 sudo apt-get install -y iputils-ping
 sudo apt-get install -y wget
 sudo apt-get install -y psmisc
 sudo apt install -y net-tools
 
 sudo apt-get install -y curl
-sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo bash
-sudo apt-get install -y nodejs
+pip3 install requests
+# sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo bash
+# sudo apt-get install -y nodejs
 
-npm install
+# npm install
